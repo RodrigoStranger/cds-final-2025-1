589c2c82fc7e27a6fb7c85d4b9e98c9d00d25f6d
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-05-30 17:40:50 -0500

    lineas

--- backend/src/routes/LineaRoute.js
+++ backend/src/routes/LineaRoute.js
@@ -3,10 +3,22 @@ const LineaController = require('../controllers/LineaController');
 
 const router = express.Router();
 
-// Ruta para verificar si ya existe una línea con el nombre especificado
+// Ruta para crear una nueva línea
+router.post('/', LineaController.AgregarLinea);
+
+// Ruta para obtener todas las líneas
+router.get('/', LineaController.ObtenerTodasLasLineas);
+
+// Ruta para obtener una línea por su ID
+router.get('/:id', LineaController.ObtenerLineaPorId);
+
+// Ruta para actualizar una línea existente
+router.put('/:id', LineaController.ActualizarLinea);
+
+// Ruta para verificar si una línea existe por nombre
 router.get('/existe-nombre/:nombre', LineaController.verificarNombreLinea);
 
-// Ruta para verificar si ya existe una línea con el id especificado
+// Ruta para verificar si una línea existe por ID
 router.get('/existe-id/:id', LineaController.verificarIdLinea);
 
 module.exports = router;
