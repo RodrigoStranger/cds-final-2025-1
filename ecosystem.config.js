41c20c8e5e8a196a609a31c14d11362331293241
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-06-24 18:47:10 -0500

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,17 @@ module.exports = {
       name: 'auth',
       script: 'auth.js',
       cwd: '/home/ec2-user/cds-final-2025-1/auth',
+      env: {
+        NODE_ENV: "production"
+     }
     },
     {
       name: 'backend',
       script: 'index.js',
       cwd: '/home/ec2-user/cds-final-2025-1/backend',
+      env: {
+        NODE_ENV: "production"
+      }
     }
   ]
 };
