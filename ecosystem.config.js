e42001cdbfd3f3c1445569d3383fe16d598e9c50
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-06-24 21:58:47 -0500

    upsate

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: './backend/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    },
+    {
+      name: 'auth',
+      script: './auth/auth.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001
+      }
+    }
+  ]
+};
